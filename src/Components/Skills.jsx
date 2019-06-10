463d14b68a9f2079d6d4918d69982f9c9ab83d64
Nick Paridon <paridonn1@gmail.com>
2019-06-10 12:14:19 -0400

    Tooltip css

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,24 +3,36 @@ import React from 'react';
 import axios from 'axios';
 
 export default class Skills extends React.Component {
-  state = {
-    skills: []
-  }
+    state = {
+        skills: []
+    }
 
-  componentDidMount() {
-    axios.get(`/api/skills`)
-      .then(res => {
-          console.log(res)
-        const skills = res.data;
-        this.setState({ skills });
-      })
-  }
+    componentDidMount() {
+        axios.get(`/api/skills`)
+            .then(res => {
+                console.log(res)
+                const skills = res.data;
+                this.setState({ skills });
+            })
+    }
 
-  render() {
-    return (
-      <ul>
-        { this.state.skills.map(skill => <li key={skill.id}>{skill.name}</li>)}
-      </ul>
-    )
-  }
+    click = () => {
+        axios.get(`/api/skills?level=4`)
+            .then(res => {
+                console.log(res)
+             
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.click}>Click</button>
+                <ul>
+                    {this.state.skills.map(skill => <div key={skill.id}><img src={skill.jpg} /> <div className="wowhead-tooltip" dangerouslySetInnerHTML={{  __html: skill.toolTip}}></div></div>)}
+                </ul>
+            </div>
+
+        )
+    }
 }
