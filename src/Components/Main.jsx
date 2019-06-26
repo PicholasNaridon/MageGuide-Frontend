686cedef56d7ecf69cdfa53c2be3f80c076f736c
Nick Paridon <paridonn1@gmail.com>
2019-06-26 09:34:33 -0400

    Image update
    A

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Container, Row, Col, ProgressBar } from 'react-bootstrap'
+import { Button, Row, Col, ProgressBar } from 'react-bootstrap'
+
 import Level from './Level'
 
 
@@ -13,32 +14,32 @@ class Main extends Component {
     }
 
     setLevel = (n) => {
-        this.setState((prevState, props) => { 
-            return {level: n}
+        this.setState((prevState, props) => {
+            return { level: n }
         })
     }
     render() {
         return (
             <Container>
-                <Row>
-                    <Col md={{ span: 6, offset: 3 }}>
-                        <h1 style={{ color: "Gold" }}>Classic Mage Guide</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Button onClick={this.setLevel.bind(this,  this.state.level + 1)}> Up </Button>
-                        <Button onClick={this.setLevel.bind(this,  this.state.level - 1)}> Down </Button>
-                        <ProgressBar now={this.state.level} max={60} min={1} striped label={`${this.state.level}`} />;
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }}>
+                            <h1 style={{ color: "Gold" }}>Classic Mage Guide</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button onClick={this.setLevel.bind(this, this.state.level + 1)}> Up </Button>
+                            <Button onClick={this.setLevel.bind(this, this.state.level - 1)}> Down </Button>
+                            <ProgressBar now={this.state.level} max={60} min={1} striped label={`${this.state.level}`} />
 
-                    </Col>
-                    <Col xl>
-                        <Level level={this.state.level} />
-                    </Col>
-                </Row>
-                <Row>
-                </Row>
-            </Container >
+                        </Col>
+                        <Col xl>
+                            <Level level={this.state.level} />
+                        </Col>
+                    </Row>
+                    <Row>
+                    </Row>
+                </Container >
         )
     }
 }
