990cf6d557b9ce9ad7830670e16b6fadf4b47f91
Nick Paridon <paridonn1@gmail.com>
2019-06-26 08:31:29 -0400

    Add RevampedTalents.jsx

---
+++ src/Components/RevampedTalents.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import Talent from './Talent'
+
+class RevampedTalents extends Component {
+    render() {
+        return (
+            <div style={{ display: "flex", flex: "row", flexFlow: "space-evenly", flexWrap: "wrap" }}>
+
+                <Talent lvl={this.props.lvl} baseId={29438} talentLvl={10} />
+                <Talent lvl={this.props.lvl} baseId={29439} talentLvl={11} />
+                <Talent lvl={this.props.lvl} baseId={11070} talentLvl={12} />
+                <Talent lvl={this.props.lvl} baseId={12473} talentLvl={13} />
+                <Talent lvl={this.props.lvl} baseId={16763} talentLvl={14} />
+                <Talent lvl={this.props.lvl} baseId={11071} talentLvl={15} />
+                <Talent lvl={this.props.lvl} baseId={12496} talentLvl={16} />
+                <Talent lvl={this.props.lvl} baseId={12497} talentLvl={17} />
+                <Talent lvl={this.props.lvl} baseId={11207} talentLvl={18} />
+                <Talent lvl={this.props.lvl} baseId={12672} talentLvl={19} />
+                <Talent lvl={this.props.lvl} baseId={15047} talentLvl={20} />
+                <Talent lvl={this.props.lvl} baseId={15052} talentLvl={21} />
+                <Talent lvl={this.props.lvl} baseId={15053} talentLvl={22} />
+                <Talent lvl={this.props.lvl} baseId={11165} talentLvl={23} />
+                <Talent lvl={this.props.lvl} baseId={12475} talentLvl={24} />
+                <Talent lvl={this.props.lvl} baseId={11170} talentLvl={25} />
+                <Talent lvl={this.props.lvl} baseId={12982} talentLvl={26} />
+                <Talent lvl={this.props.lvl} baseId={12983} talentLvl={27} />
+                <Talent lvl={this.props.lvl} baseId={12984} talentLvl={28} />
+                <Talent lvl={this.props.lvl} baseId={12985} talentLvl={29} />
+                <Talent lvl={this.props.lvl} baseId={11958} talentLvl={30} />
+                <Talent lvl={this.props.lvl} baseId={16765} talentLvl={31} />
+                <Talent lvl={this.props.lvl} baseId={16766} talentLvl={32} />
+                <Talent lvl={this.props.lvl} baseId={12472} talentLvl={33} />
+                <Talent lvl={this.props.lvl} baseId={11160} talentLvl={34} />
+                <Talent lvl={this.props.lvl} baseId={12518} talentLvl={35} />
+                <Talent lvl={this.props.lvl} baseId={12519} talentLvl={36} />
+                <Talent lvl={this.props.lvl} baseId={16757} talentLvl={37} />
+                <Talent lvl={this.props.lvl} baseId={16758} talentLvl={38} />
+                <Talent lvl={this.props.lvl} baseId={29440} talentLvl={39} />
+                <Talent lvl={this.props.lvl} baseId={11426} talentLvl={40} />
+                <Talent lvl={this.props.lvl} baseId={11210} talentLvl={41} />
+                <Talent lvl={this.props.lvl} baseId={12592} talentLvl={42} />
+                <Talent lvl={this.props.lvl} baseId={11222} talentLvl={43} />
+                <Talent lvl={this.props.lvl} baseId={12839} talentLvl={44} />
+                <Talent lvl={this.props.lvl} baseId={12840} talentLvl={45} />
+                <Talent lvl={this.props.lvl} baseId={11213} talentLvl={46} />
+                <Talent lvl={this.props.lvl} baseId={12574} talentLvl={47} />
+                <Talent lvl={this.props.lvl} baseId={12575} talentLvl={48} />
+                <Talent lvl={this.props.lvl} baseId={12576} talentLvl={49} />
+                <Talent lvl={this.props.lvl} baseId={12577} talentLvl={50} />
+                <Talent lvl={this.props.lvl} baseId={11151} talentLvl={51} />
+                <Talent lvl={this.props.lvl} baseId={12952} talentLvl={52} />
+                <Talent lvl={this.props.lvl} baseId={12953} talentLvl={53} />
+                <Talent lvl={this.props.lvl} baseId={6057} talentLvl={54} />
+                <Talent lvl={this.props.lvl} baseId={6085} talentLvl={55} />
+                <Talent lvl={this.props.lvl} baseId={28574} talentLvl={56} />
+                <Talent lvl={this.props.lvl} baseId={11242} talentLvl={57} />
+                <Talent lvl={this.props.lvl} baseId={12467} talentLvl={58} />
+                <Talent lvl={this.props.lvl} baseId={18462} talentLvl={59} />
+                <Talent lvl={this.props.lvl} baseId={18463} talentLvl={60} />
+
+            </div>
+        )
+    }
+}
+
+export default RevampedTalents
