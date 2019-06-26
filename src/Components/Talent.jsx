096cf56b9625e329f2044652427f014f039babcc
Nick Paridon <paridonn1@gmail.com>
2019-06-26 08:31:55 -0400

    Refactor  Talent.jsx to use RevampedTalents logic

--- src/Components/Talent.jsx
+++ src/Components/Talent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Tooltip, OverlayTrigger } from 'react-bootstrap'
+import { Tooltip, OverlayTrigger, Spinner } from 'react-bootstrap'
 import _ from 'lodash';
 import axios from 'axios'
 
@@ -10,9 +10,8 @@ class Talent extends Component {
         this.state = {
             talentId: this.props.baseId,
             talent: null,
-            color: null
+            color: false
         }
-        this.getTalent = this.getTalent.bind(this)
         this.checkTalentsVsLevl = this.checkTalentsVsLevl.bind(this)
     }
     componentDidUpdate(prevProps) {
@@ -22,42 +21,29 @@ class Talent extends Component {
         }
     }
     componentDidMount() {
+        axios.get(`/api/talents/${this.props.baseId}`)
+        .then(res => {
+            const talent = res.data[0];
+            this.setState({ talent: talent});
+        })
         this.checkTalentsVsLevl()
 
     }
 
-    getTalent(id, color) {
-        axios.get(`/api/talents/${id}`)
-            .then(res => {
-                const talent = res.data[0];
-                this.setState({ talent: talent, color: color });
+    checkTalentsVsLevl() {
+
+        if (this.props.talentLvl <= this.props.lvl){
+            this.setState({
+                color: true
             })
-    }
-
-    checkTalentsVsLevl(lvl) {
-        var talents = this.props.idsWithLevel
-        var maxId = this.props.baseId
-
-        if (talents != null) {
-            console.log(Object.entries(talents))
-            Object.entries(talents).forEach(entry => {
-                let key = parseInt(entry[0]);
-                let value = entry[1];
-                console.log(key, value, lvl)
-                if (lvl >= key ) {
-                    maxId = key
-                    this.getTalent(value, true)
-                }
-            });
         }
-        if (maxId == this.props.baseId)
-        this.getTalent(this.state.talentId, false)
+        
     }
 
     render() {
         if (this.state.talent) {
             return (
-                <div style={{ margin: "1%" }}>
+                <div style={{width: "10%", boxSizing: "border-box" }}>
                     <OverlayTrigger key={this.state.talent.id} trigger="hover" overlay={<Tooltip id="tooltip-disabled"> <div className="wowhead-tooltip" dangerouslySetInnerHTML={{ __html: this.state.talent.toolTip }}></div></Tooltip>}>
                         <img style={this.state.color ? null : { filter: "grayscale(100%)" }} src={this.state.talent.jpg}>
                         </img>
@@ -65,7 +51,7 @@ class Talent extends Component {
                 </div>
             )
         } else {
-            return <div />
+            return <Spinner animation="border" />
         }
     }
 }
