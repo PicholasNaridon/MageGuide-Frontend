aaa71183e48f640d099717364ed7831afa674581
Nick Paridon <paridonn1@gmail.com>
2019-06-28 09:38:54 -0400

    Hide if value is 0 on price

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
 class Price extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <span style={{padding: "3px", color: "white"}}>
-                        <img src="https://wow.zamimg.com/images/icons/money-gold.gif"></img>  {this.props.cost.gold}
-                </span>
-                <span style={{padding: "3px", color: "white"}}>
-                        <img src="https://wow.zamimg.com/images/icons/money-silver.gif"></img>  {this.props.cost.silver}
-                </span>
-                <span style={{padding: "3px", color: "white"}}>
-                        <img src="https://wow.zamimg.com/images/icons/money-copper.gif"></img>  {this.props.cost.copper}
-                </span>
+                {this.props.cost.gold > 0 ? <span style={{ padding: "3px", color: "white" }}>
+                    <img src="https://wow.zamimg.com/images/icons/money-gold.gif"></img>  {this.props.cost.gold}
+                </span> : <span></span>}
+                {this.props.cost.silver > 0 ? <span style={{ padding: "3px", color: "white" }}>
+                    <img src="https://wow.zamimg.com/images/icons/money-silver.gif"></img>  {this.props.cost.silver}
+                </span> : <span></span>}
+                {this.props.cost.copper > 0 ? <span style={{ padding: "3px", color: "white" }}>
+                    <img src="https://wow.zamimg.com/images/icons/money-copper.gif"></img>  {this.props.cost.copper}
+                </span> : <span></span>}
             </div>
         )
     }
