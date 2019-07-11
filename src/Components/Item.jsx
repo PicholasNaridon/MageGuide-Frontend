e503f0e6ddcf02319311dc6bddc5d737017727f3
Nick Paridon <paridonn1@gmail.com>
2019-07-11 08:41:57 -0400

    Fix broken image in item.jsx

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -8,14 +8,12 @@ class Item extends Component {
         this.state = {
             item: {}
         }
-        console.log(this.props)
 
     }
 
     componentDidMount() {
         axios.get(`/api/items/${this.props.id}`)
             .then(res => {
-                console.log(res)
                 this.setState({
                     item: res.data,
                 })
@@ -25,21 +23,23 @@ class Item extends Component {
     render() {
         if (this.state.item) {
             return (
-                <div style={{ width: "60px", height: "60px", margin: "auto", boxSizing: "border-box"}} >
-                <OverlayTrigger
-                    key={this.state.item.id}
-                    trigger="hover"
-                    overlay={
-                        <Tooltip id="tooltip-disabled">
-                            <div
-                                className="wowhead-tooltip"
-                                dangerouslySetInnerHTML={{ __html: this.state.item.toolTip }}>
-                            </div>
-                        </Tooltip>}
-                >
-                    <img src={`https://wow.zamimg.com/images/wow/icons/large/${this.state.item.icon}.jpg`} >
-                    </img>
-                </OverlayTrigger>
+                <div style={{ width: "60px", height: "60px", margin: "auto", boxSizing: "border-box" }} >
+                    <OverlayTrigger
+                        key={this.state.item.id}
+                        trigger="hover"
+                        overlay={
+                            <Tooltip id="tooltip-disabled">
+                                <div
+                                    className="wowhead-tooltip"
+                                    dangerouslySetInnerHTML={{ __html: this.state.item.toolTip }}>
+                                </div>
+                            </Tooltip>}
+                    >
+                        {this.state.item.icon ?
+                            <img src={`https://wow.zamimg.com/images/wow/icons/large/${this.state.item.icon}.jpg`} ></img> :
+                            <div></div>
+                        }
+                    </OverlayTrigger>
                 </div>
             )
         } else {
